ee04cc2f68739e1b8546e32cc5d4a5c77d003fe5
Leonardo Ramírez <juanlov4321@gmail.com>
2025-11-19 15:36:29 -0300

    Add token constants for ETH and USDC

---
+++ app/const/token.ts
@@ -0,0 +1,16 @@
+export const TOKENS = {
+  ETH: {
+    address: "0x0000000000000000000000000000000000000000",
+    abi: [],
+    logo: "https://cryptologos.cc/logos/ethereum-eth-logo.png?v=023",
+    name: "Ethereum",
+    symbol: "ETH",
+  },
+  USDC: {
+    address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+    abi: [],
+    logo: "https://cryptologos.cc/logos/usd-coin-usdc-logo.png?v=023",
+    name: "USD Coin",
+    symbol: "USDC",
+  },
+};
