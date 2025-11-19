e1ad7d467250d5665fe131d8a7458cbd09b9ee54
Leonardo Ramírez <juanlov4321@gmail.com>
2025-11-19 15:35:57 -0300

    Add Navbar component

---
+++ app/components/navbar/index.tsx
@@ -0,0 +1,14 @@
+import LogoSwapGuard from "@/app/assets/logoswapguard.png";
+const Navbar = () => {
+  return (
+    <div className="navbar ">
+      <div className="navbar-start"></div>
+      <div className="navbar-center">
+        <a className="btn btn-ghost text-xl">Swap Guard</a>
+      </div>
+      <div className="navbar-end"></div>
+    </div>
+  );
+};
+
+export default Navbar;
