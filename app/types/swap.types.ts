99e8d430dfd34d929ce61d2b9d24c65cadfc1572
Leonardo Ramírez <juanlov4321@gmail.com>
2025-11-20 11:33:18 -0300

    Refactor ISwap interface to ISwapGuardSubscription with additional properties for subscription management

--- app/types/swap.types.ts
+++ app/types/swap.types.ts
@@ -1,5 +1,9 @@
-export interface ISwap {
-  address: string;
-  tokenFrom: string;
-  tokenTo: string;
+export interface ISwapGuardSubscription {
+	address: string;
+	tokenFrom: string;
+	tokenTo: string;
+	hourToSendNotification: number;
+	amountToSubscribe: number;
+	chatIdTelegram: string;
+	percentageOfSwap: number;
 }
